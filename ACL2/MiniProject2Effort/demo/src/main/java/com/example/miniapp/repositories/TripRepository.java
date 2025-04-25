c24fa57f7c256fce0f17f190b5d541af5dad8a1e
andrewSky18 <116117989+andrewSky18@users.noreply.github.com>
2025-04-25 23:20:04 +0300

    Update TripRepository.java

--- ACL2/MiniProject2Effort/demo/src/main/java/com/example/miniapp/repositories/TripRepository.java
+++ ACL2/MiniProject2Effort/demo/src/main/java/com/example/miniapp/repositories/TripRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Repository
 public interface TripRepository extends JpaRepository<Trip, Long> {
     List<Trip> findByTripDateBetween(LocalDateTime startDate, LocalDateTime endDate);
     List<Trip> findByCaptainId(Long captainId);
