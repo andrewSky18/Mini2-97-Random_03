56f4045a0fc161a6b51221ac1646f25e357f6982
andrewSky18 <116117989+andrewSky18@users.noreply.github.com>
2025-04-25 23:19:24 +0300

    Update CustomerRepository.java

--- ACL2/MiniProject2Effort/demo/src/main/java/com/example/miniapp/repositories/CustomerRepository.java
+++ ACL2/MiniProject2Effort/demo/src/main/java/com/example/miniapp/repositories/CustomerRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
     List<Customer> findByEmailEndingWith(String domain);
     List<Customer> findByPhoneNumberStartingWith(String prefix);
