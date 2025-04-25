42a469584e9d9e09f9e6e1e408f7ee0f8530d357
andrewSky18 <116117989+andrewSky18@users.noreply.github.com>
2025-04-25 23:19:53 +0300

    Update RatingRepository.java

--- ACL2/MiniProject2Effort/demo/src/main/java/com/example/miniapp/repositories/RatingRepository.java
+++ ACL2/MiniProject2Effort/demo/src/main/java/com/example/miniapp/repositories/RatingRepository.java
@@ -4,7 +4,7 @@ import com.example.miniapp.models.Rating;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
-
+@Repository
 public interface RatingRepository extends MongoRepository<Rating, String> {
     List<Rating> findByEntityIdAndEntityType(Long entityId, String entityType);
     List<Rating> findByScoreGreaterThan(int score);
